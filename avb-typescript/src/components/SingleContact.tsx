a076b22e5b39e4ffda5204052117d7c9b2dde8fb
Alex Williams <Zizzs17@gmail.com>
2021-09-12 13:12:55 -0700

    Buttons and Email Adding
    Added the ability for the user to add an email. This uses an Input Email button that changes to an Add Email button when clicked. When the user types in an email into the input line and clicks add email, the new email is shown on the page and the Add Email button changes back to the Input Email button.
    
    Added Delete and Save buttons for future use.
    
    CSS for all buttons added.

--- avb-typescript/src/components/SingleContact.tsx
+++ avb-typescript/src/components/SingleContact.tsx
@@ -5,6 +5,8 @@ import { ContactData, SingleContactData } from "../types/ContactData";
 const SingleContact: React.FC<SingleContactData> = (props) => {
 
   const [mutableContact, setMutableContact] = useState<ContactData>();
+  const [inputEmailVisibility, setInputEmail] = useState<boolean>(false);
+  const [inputtedEmailTemp, setInputtedEmailTemp] = useState<string>("");
 
   let tempContact: ContactData = {
     id: props.nextContactID,
@@ -13,49 +15,76 @@ const SingleContact: React.FC<SingleContactData> = (props) => {
     emails: [],
   };
 
+  // If the contact passed down exists and we have no mutable contact created, then we want to use the real contact information.
   if (props.singleContact !== undefined && mutableContact === undefined){
     setMutableContact(props.singleContact);
   }
 
+  // If the contact passed down exists, and we already have a mutable contact but the ids are different, replace the old data with the new contact data.
   if (props.singleContact !== undefined && mutableContact !== undefined){
     if(props.singleContact.id !== mutableContact.id){
       setMutableContact(props.singleContact);
     }
   }
 
+  // If the single contact is undefined (If the user clicks the same contact again to deselect it), and we still have mutable contact data from the previous contact, set the mutable contact data to the temporary contact information (Can be used to make a new contact profile)
   if (props.singleContact === undefined && mutableContact !== undefined){
     if(mutableContact.id !== tempContact.id){
       setMutableContact(tempContact);
     }
   }
 
+  // Only happens when the page is loaded for the first time. When the user has not clicked on a contact yet, causing the mutable contact data to be undefined as well. Set the mutable contact data to the temporary contact. (Can be used to make a new contact profile).
   if ( props.singleContact === undefined && mutableContact === undefined) {
     setMutableContact(tempContact);
   }
 
-
+  // Function called upon any change to first name input, modifies the mutable contact.
   const editFirstName = (value: string) => {
     if(mutableContact !== undefined){
-      tempContact = {...mutableContact}
+      tempContact = {...mutableContact};
       tempContact.firstName = value;
       setMutableContact(tempContact);
     }
     console.log(mutableContact);
   }
 
+  // Function called upon any change to the last name input, modifies the mutable contact.
   const editLastName = (value: string) => {
     if(mutableContact !== undefined){
-      tempContact = {...mutableContact}
+      tempContact = {...mutableContact};
       tempContact.lastName = value;
       setMutableContact(tempContact);
     }
     console.log(mutableContact);
   }
 
+  const addEmail = () => {
+    if(mutableContact !== undefined){
+      tempContact = {...mutableContact};
+      tempContact.emails.push(inputtedEmailTemp);
+      setMutableContact(tempContact);
+      setInputtedEmailTemp("");
+      setInputEmail(false);
+    }
+  }
+
+  const handleInputEmailChange = (value: string) => {
+    setInputtedEmailTemp(value);
+  }
+
+  const handleInputEmailButton = () => {
+    setInputEmail(true);
+  }
+
+  const handleAddEmailButton = () => {
+    setInputEmail(false);
+  }
+
   let key = 0;
   if (mutableContact !== undefined){
     return (
-      <div>
+      <div className={"single-contact-root-div"}>
         <div className={"single-contact-first-last-name-div"}>
           <div>
             <p>First Name</p>
@@ -66,17 +95,46 @@ const SingleContact: React.FC<SingleContactData> = (props) => {
             <input className={"single-contact-input"} type="text" value={`${mutableContact.lastName}`} onChange={(e: React.ChangeEvent<HTMLInputElement>): void => (editLastName(e.target.value))}></input>
           </div>
         </div>
-        <div>Emails: {
-          mutableContact.emails.map((email) => {
-            return (<p key={key += 1}>{email}</p>)
-          })
-        }</div>
+        <div className={"single-contact-emails-div"}>
+          <p className={"single-contact-emails-header"}>Emails:</p>
+          <div className={"single-contact-emails"}>
+            {
+            mutableContact.emails.map((email) => {
+              return (<p key={key += 1}>{email}</p>)
+            })
+            }
+            {
+              inputEmailVisibility ? 
+              <div>
+                <input onChange={(e: React.ChangeEvent<HTMLInputElement>): void => (handleInputEmailChange(e.target.value))}/>
+                <p className={"single-contact-input-email-button"} onClick={(event: React.MouseEvent<HTMLElement>) => {
+                  addEmail(); 
+                  handleAddEmailButton();
+                }}>Add Email</p>
+              </div> :
+              <div>
+                <p className={"single-contact-input-email-button"} onClick={(event: React.MouseEvent<HTMLElement>) => { 
+                  handleInputEmailButton();
+                }}>Input Email</p>
+              </div>
+            }
+          </div>
+        </div>
+        <div className={"single-contact-interact-buttons"}>
+          {mutableContact.id === props.nextContactID ?
+            <p className={"single-contact-add-button"}>Add</p> :
+            <p className={"single-contact-delete-button"}>Delete</p>
+
+          }
+          <p></p>
+          <p className={"single-contact-save-button"}>Save</p>
+        </div>
       </div>
     ) 
   } else {
     return (
       <div>
-        No Contacts Selected
+        <p>You shouldn't be here. Click a contact to view information.</p>
       </div>
     ) 
   }
